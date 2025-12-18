e154bf7072ee7f587c07532a76466a525f486180
Kahthiravan <A202584@siswa.ukm.edu.my>
2025-12-18 09:15:57 +0800

    e

--- logout.php
+++ logout.php
@@ -1,7 +1,22 @@
 <?php
 session_start();
-session_unset();
+
+// 1. Unset all session values
+$_SESSION = array();
+
+// 2. Delete the session cookie if it exists
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// 3. Destroy the session
 session_destroy();
+
+// 4. Redirect to Login Page
 header("Location: index.php");
 exit();
 ?>
