e0bab3a4f351b11c3d352fbf46bc94988f1e172e
A203914 <a203914@siswa.ukm.edu.my>
2026-01-01 21:18:43 +0800

    f

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -38,8 +38,7 @@ $nav_active = $nav_active ?? '';
                 <span class="absolute -bottom-1 left-0 w-full h-0.5 bg-[#0b4d9d] rounded-full"></span>
                 <?php endif; ?>
             </a>
-
-            <!-- Feedback -->
+             <!-- Feedback -->
             <a href="view_feedback.php" class="<?php echo ($nav_active === 'feedback') ? 'text-[#0b4d9d] font-bold flex items-center gap-2 relative' : 'text-slate-500 hover:text-[#0b4d9d] font-semibold transition'; ?>">
                 Feedback
                 <?php if($nav_active === 'feedback'): ?>
@@ -47,7 +46,17 @@ $nav_active = $nav_active ?? '';
                 <?php endif; ?>
             </a>
 
-            
+            <!-- Notifications -->
+            <div class="relative cursor-pointer">
+                <a href="notification.php" class="flex items-center text-slate-500 hover:text-[#0b4d9d] transition relative">
+                    <i class="fa-solid fa-bell text-xl"></i>
+                    <span id="notif-count"
+                          class="hidden absolute -top-1 -right-2 bg-red-600 text-white text-xs font-bold 
+                                 rounded-full w-5 h-5 flex items-center justify-center">
+                    </span>
+                </a>
+            </div>
+
             <!-- User Profile -->
             <div class="flex items-center gap-4 pl-6 border-l border-slate-200">
                 <div class="text-right hidden sm:block">
